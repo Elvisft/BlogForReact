70760b769a861c361b0d28dc9edbf1c6e2f369c4
刘军辉 <oe52920@gmail.com>
2017-08-10 19:09:23 +0800

    `123456`

--- src/about/about.tsx
+++ src/about/about.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { Icon } from 'antd';
 import * as actions from './../actions';
 
 import './about.css';
+
+let aboutbg = require('./../static/img/about_0.png');
 // 关于
 class About extends React.Component<any, any> {
 
@@ -17,11 +20,21 @@ class About extends React.Component<any, any> {
             <div className="about">
                 <div className="about-page" >
                     <div className="color-1 about-title text-left vertical">
-                        <div className="font-5">GEEK EDGE</div>
-                        <div className="font-2">极客的极限在哪里？边缘在哪里？无人知晓，我们必将永远追寻极限，到达那个边缘</div>
+                        <img src={aboutbg} alt=""/>
+                        <div className="font-2">极客的极限在哪里？</div>
+                        <div className="font-2">边缘在哪里？</div>
+                        <div className="font-2">无人知晓</div>
+                        <div className="font-2">我们必将永远追寻极限</div>
+                        <div className="font-2">到达那个边缘</div>
+                        <div className="font-2">///</div>
                     </div>
+                    <a href="#content" className="about-down">
+                        <Icon type="down" />
+                    </a>
                 </div>
-                <div className="about-page"></div>
+                <div id="content" className="about-page">
+                    123
+                </div>
                 <div className="about-page"></div>
             </div>
         );
