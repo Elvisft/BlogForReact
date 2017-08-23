f2c7a731b390609f5d3c5abd6be1a12d245cdffd
刘军辉 <oe52920@gmail.com>
2017-08-23 16:27:47 +0800

    `123456`

--- server/routes/article.js
+++ server/routes/article.js
@@ -1,10 +1,3 @@
-var express = require('express');
-var router = express.Router();
-//
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
 exports.autowired = {
     'get' : {
         '/' : (req, res, next)=>{
--- server/util/routeUtil.js
+++ server/util/routeUtil.js
@@ -1,14 +1,12 @@
-let fs = require('fs'),extend = require('extend');
-let path = require('path');
-let express = require('express');
-let router = express.Router();
+let fs = require('fs'),extend = require('extend'),path = require('path'),router = require('express').Router();
+
 
 let config={
-    "class-route-path":false,
-    "scan-path":"routes",
-    "route-property":"autowired"
+    "class-route-path":false,//是否启动js级路径
+    "scan-path":"routes",//route存放路径
+    "route-property":"autowired"//自动注入依赖的module属性名
 }
-
+//获取项目根路径
 let getRootPath=(temPath)=>{
     while (true){
         let file= fs.readdirSync(temPath);
@@ -21,30 +19,21 @@ let getRootPath=(temPath)=>{
         else temPath=path.dirname(temPath);
     }
 }
+
 let loadFile = (filePath, path)=> {
     let routeObj = require(filePath),jsPath='';
     if(config["class-route-path"]) jsPath='/'+path.match(/\w*/).toString();
-    //如果包含autoroute属性，则进行解析
     if (routeObj[config["route-property"]]) {
-
         for (let method in routeObj[config["route-property"]]) {
             let routeList = routeObj[config["route-property"]][method];
-
-            if (!routeList) {
-                break ;
-            }
-
-            //method就是上面取到的get、post
-
+            if (!routeList) break ;
             for(let routeRule in routeList) {
-                //func获取得到的就是上面对应各项的处理函数
                 let func = routeList[routeRule];
                 router[method](jsPath+routeRule, func);
             }
         }
     }
 }
-
 
 let rootPath=getRootPath(path.normalize(path.dirname(require.main.filename)));
 let routesPath =rootPath+'/'+config["scan-path"];
@@ -56,24 +45,15 @@ fs.readdir(routesPath, (err, file)=>{
     if(err){
         return;
     }
-
     file.map((path) => {
         let filepath=routesPath+'/'+path;
         fs.stat(filepath, (err, stats)=> {
-            if (err) {
-                return ;
-            }
-
-            if (stats.isDirectory()) {
-                //递归执行函数
-            } else  {
-                //加载文件并解析
+            if (err) return ;
+            if (stats.isFile()) {
                 loadFile(filepath, path);
             }
         })
     });
 });
 
-
 module.exports = router;
-
