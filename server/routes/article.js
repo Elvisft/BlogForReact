c32851a061302e71b8c4c3745c73e088c095499d
刘军辉 <oe52920@gmail.com>
2017-09-11 19:26:18 +0800

    `123456`

--- server/routes/article.js
+++ server/routes/article.js
@@ -1,7 +1,12 @@
 exports.autowired = {
     'get' : {
-        '/' : (req, res, next)=>{
-            res.send('respond with a resource');
+        '/getArticle/:type/:page/size' : (req, res, next)=>{
+            req.getConnection((err, conn) => {
+                conn.query('SELECT * FROM article WHERE type=? ORDER BY date DESC LIMIT ?,?',
+                    [req.param.type, req.param.page, req.param.size], (err, result) => {
+                        console.log(result)
+                    });
+            })
         }
     },
     'post' : {
