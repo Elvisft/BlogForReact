9a33ab14238a2919a70e1a6b9f168ca10c5b5665
刘军辉 <oe52920@gmail.com>
2017-09-11 19:35:55 +0800

    `123456`

--- server/routes/article.js
+++ server/routes/article.js
@@ -1,10 +1,16 @@
 exports.autowired = {
     'get' : {
-        '/getArticle/:type/:page/size' : (req, res, next)=>{
+        '/getArticle/:type/:page/:size' : (req, res, next)=>{
+            console.log([typeof req.params.type, typeof req.params.page, typeof req.params.size])
             req.getConnection((err, conn) => {
                 conn.query('SELECT * FROM article WHERE type=? ORDER BY date DESC LIMIT ?,?',
-                    [req.param.type, req.param.page, req.param.size], (err, result) => {
-                        console.log(result)
+                    [parseInt(req.params.type), parseInt(req.params.page), parseInt(req.params.size)], (err, result) => {
+                        if(err){
+                            res.json(err);
+                        }else{
+                            console.log(result)
+                            res.json(result);
+                        }
                     });
             })
         }
