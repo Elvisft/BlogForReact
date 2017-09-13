2e3b866947d9f75e57efcfd64e95a0d7fa954f8c
MR-Liu <oe52920@gmail.com>
2017-09-14 01:10:02 +0800

    init

--- client/main.js
+++ client/main.js
@@ -1,11 +1,2 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import productBox from './components/productBox';
-
-
-ReactDom.render(
-    <div>
-        123123
-    </div>,
-    document.getElementById('react')
-);
+var greeter = require('./Greeter.js');
+document.getElementById('root').appendChild(greeter());
--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,43 @@
-/**
- * Created by chinacscs on 2017/9/13.
- */
-var webpack = require('webpack');
-var path = require('path');
-console.log(123);
-// module.exports = {
-//     //页面入口文件配置
-//     entry: {
-//         index: [
-//             'webpack-dev-server/client?http://localhost:5000',
-//             'webpack/hot/only-dev-server',
-//             './client/main.js'
-//         ]
-//     },
-//     //入口文件输出配置
-//     output: {
-//         path: __dirname + '/assets/',
-//         filename: 'bundle.js'
-//     },
-//     module: {
-//         //加载器配置
-//         loaders: [
-//             {
-//                 test: /\.css$/,
-//                 loader: 'style-loader!css-loader'
-//             },
-//
-//             {
-//                 test: /\.js$/,
-//                 loader: 'jsx-loader?harmony'
-//             },
-//             {
-//                 test: /\.(png|jpg)$/,
-//                 loader: 'url-loader?limit=8192'
-//             },
-//             {
-//                 test: /\.js|jsx$/,
-//                 loaders: ['react-hot', 'babel?presets[]=es2015,presets[]=react,presets[]=stage-0'],
-//                 include: path.join(__dirname, 'js')
-//             }
-//         ]
-//     },
-//     //其它解决方案配置
-//     resolve: {
-//         extensions: ['', '.js', '.json', '.scss']
-//     },
-//     //插件项
-//     plugins: [
-//         new webpack.HotModuleReplacementPlugin(),
-//         new webpack.NoErrorsPlugin()
-//     ]
-// };
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: ['webpack/hot/dev-server', path.resolve(__dirname, './assets/main.js')],
+    entry:  __dirname + "/client/main.js",
     output: {
-        path: path.resolve(__dirname, './assets/'),
-        filename: 'bundle.js',
+        path: __dirname + "/build",
+        filename: "bundle.js"
     },
+    devtool: 'eval-source-map',
+
     module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            //loader: 'babel?presets[]=es2015&presets[]=react'
-            loader: 'babel-loader',
-            query: {
-                presets: [["es2015", { "loose": true }],'react','stage-0'],
-                // plugins: ["transform-es5-property-mutators","transform-jscript","transform-es3-property-literals","transform-es3-member-expression-literals",]
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            }, {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!less-loader',
+            }, {
+                test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+                loader: 'file-loader'
+            }, {
+                test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+                loader: 'file-loader',
+                query: {
+                    name: '[name].[ext]?[hash]'
+                }
             }
-        }, {
-            test: /\.css$/,
-            exclude: /node_modules/,
-            loader: "style!css",
-            //loader: ExtractTextPlugin.extract('style', 'css')
-        }, {
-            test: /\.less$/,
-            exclude: /node_modules/,
-            loader: "style!css!less",
-            //loader: ExtractTextPlugin.extract('style', 'css!less')
-        }, {
-            test: /\.(png|jpg|jpeg|gif)$/,
-            exclude: /node_modules/,
-            loader: 'url?limit=10000&name=img/[name].[hash].[ext]'
-        }, {
-            test: /\.woff|\.woff2|\.svg|.eot|\.ttf/,
-            exclude: /node_modules/,
-            loader: 'url?prefix=font/&limit=10000&name=font/[name].[ext]'
-        }],
-    }
-};
+        ]
+    },
+
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './client/public/index.html',
+            title:'123',
+            minify:{
+                removeComments:true,
+                collapseWhitespace:true
+            }
+        })
+    ]
+}
