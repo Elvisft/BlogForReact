f09e09b3fb25ded921b624c6a5c97dded7012ade
刘军辉 <oe52920@gmail.com>
2017-10-13 18:22:12 +0800

    123456

---
+++ client/manager/manager.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Menu, Icon, Button } from 'antd';
+const SubMenu = Menu.SubMenu;
+
+class Manager extends React.Component{
+    render(){
+        return (
+            <div>123</div>
+        )
+    }
+}
+export default Manager;
